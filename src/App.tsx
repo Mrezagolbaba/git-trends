856ab9d55f80511d3b377f12fed3832151d3f725
Mohamadreza <m.reza.golbaba@gmail.com>
2020-08-27 00:20:10 +0430

    Home:create App function

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import FilterContent from "./Components/Filter";
+import SelectList from "./Components/Filter";
 import CardContent from './Components/CardContent';
 
+
 function App() {
+    const [selected, setSelected] = React.useState('');
+    const handleIdChange = React.useCallback(newId => {
+        setSelected(selected);
+    }, []);
+
   return (
     <div className="App">
-         <FilterContent/>
-         <CardContent/>
+         <SelectList handleIdChange={handleIdChange}/>
+         <CardContent value ={selected} />
     </div>
   );
 }
