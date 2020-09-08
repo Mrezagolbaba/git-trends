4871a4cccd8e76c7ed5c477088c7d30e061a5137
Mohamadreza <m.reza.golbaba@gmail.com>
2020-09-09 01:03:29 +0430

    Card: add card info

--- src/Components/CardContent/Card.tsx
+++ src/Components/CardContent/Card.tsx
@@ -11,17 +11,25 @@ const Card: React.FC<Props> = ({data, ...rest})=>{
     if (!data.search ) {
         return <div>No trends available</div>;
     }
+
    const info = data.search.edges?.map(i=>{
        return i?.node
    })
-console.log(info)
+
     return(
         <div>
-
-
-
-            {/* {} */}
-            {/*})}*/}
+                {info?.map((i)=>{
+                if(i?.__typename === "Repository"){
+                   return (
+                   <div className="flex justify-center items-center">
+                             <span>{i.forkCount}</span>
+                            <span>{i.homepageUrl!==""?i.homepageUrl:""}</span>
+                            <span>{i.issues.__typename==="IssueConnection"? i.issues.totalCount:''}</span>
+                   </div>
+                   
+                    )
+                    // 
+                }})}
         </div>
     )
 }
